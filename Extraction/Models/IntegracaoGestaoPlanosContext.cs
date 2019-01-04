5180eaab2ab68ee411801aaef3465b123fb066d0
ricardokanazawa <ricardokanazawa@gmail.com>
2019-01-03 22:24:14 -0200

    Update IntegracaoGestaoPlanosContext.cs

--- Extraction/Models/IntegracaoGestaoPlanosContext.cs
+++ Extraction/Models/IntegracaoGestaoPlanosContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 
-namespace FpGrowthCore.Models
+namespace Extraction.Models
 {
     public class IntegracaoGestaoPlanosContext : DbContext
     {

de5efb8253c39fd2a9f7918f3d6d73bdd0d0a26a
ricardokanazawa <ricardokanazawa@gmail.com>
2019-03-15 13:38:00 -0300

    Update Program.cs

--- Extraction/Program.cs
+++ Extraction/Program.cs
@@ -1,17 +1,17 @@
-using Extration.Export;
-using Extration.Models;
+using FpGrowthCore.Export;
+using FpGrowthCore.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using static Extration.Models.IntegracaoGestaoPlanosContext;
+using static FpGrowthCore.Models.IntegracaoGestaoPlanosContext;
 
-namespace Extration
+namespace FpGrowthCore
 {
     class Program
     {
         static void Main(string[] args)
-        {   
+        {
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
             sw.Start();
 
@@ -21,39 +21,55 @@ namespace Extration
 
             using (var db = new IntegracaoGestaoPlanosContext())
             {
-                List<TransacaoBeneficiario> lst = db.Transacoes.FromSql("[SP_TRANSACOES_EXP] @Id_Prestador = {0}, @Dt_inicio = {1}, @Dt_fim = {2}, @Cd_Procedimento = {3}",
-                    1, new DateTime(2017, 1, 1), new DateTime(2018, 1, 1), "60002450").ToList();
-
-                foreach (var transacao in lst)
+                for (int i = 1; i <= 12; i++)
                 {
-                    List<TransacaoBeneficiario> lst2 = db.Transacoes.FromSql("[SP_TRANSACOES_EXP] @Id_Prestador = {0}, @Dt_inicio = {1}, @Dt_fim = {2}, @Carteira = {3}",
-                    1, new DateTime(2017, 1, 1), new DateTime(2018, 1, 1), transacao.Carteira).ToList();
+                    var dataIni = new DateTime(2017, i, 1);
+                    var dataFim = dataIni.AddMonths(1).AddSeconds(-1);
 
-                    foreach (var transacaoFim in lst2.Where(l => l.DataExecucao <= transacao.DataExecucao))
+                    List<TransacaoBeneficiario> lst = db.Transacoes.FromSql("[SP_TRANSACOES_EXP] @Id_Prestador = {0}, @Dt_inicio = {1}, @Dt_fim = {2}",
+                    2, dataIni, dataFim).ToList();
+
+                    foreach (var transacao in lst.GroupBy(x => x.NumeroGuia))
                     {
-                       Claim claim = null;
-                        var claimService = new ClaimService { ProcedureCode = transacaoFim.CodigoProcedimento, TableCode = transacaoFim.CodigoTabela.ToString(), UnitCost = transacaoFim.ValorUnitario };
+                        if (transacao.FirstOrDefault(s => s.CodigoTabela == 22) == null)
+                            continue;
 
-                        if ((claim = claims2.FirstOrDefault(c => c.AuthorizationId == transacaoFim.Carteira)) != null)
+                        if (transacao.Count() == 1 || transacao.Count() > 50)
+                            continue;
+
+                        if (transacao.All(s => s.CodigoTabela == 22))
+                            continue;                        
+
+                        foreach (var servico in transacao)
                         {
-                            if (!claim.Services.Any(s => s.Key == claimService.Key))
-                                claim.Services.Add(claimService);
-                        }
-                        else
-                        {
-                            claims2.Add(new Claim
+                            Claim claim = null;
+                            var claimService = new ClaimService { ProcedureCode = servico.CodigoProcedimento, TableCode = servico.CodigoTabela.ToString(), UnitCost = servico.ValorUnitario };
+
+                            if ((claim = claims2.FirstOrDefault(c => c.AuthorizationId == transacao.Key)) != null)
                             {
-                                AuthorizationId = transacaoFim.Carteira,
-                                Services = new HashSet<ClaimService> { claimService }
-                            });
+                                if (!claim.Services.Any(s => s.Key == claimService.Key))
+                                    claim.Services.Add(claimService);
+                            }
+                            else
+                            {
+                                claims2.Add(new Claim
+                                {
+                                    AuthorizationId = transacao.Key,
+                                    Services = new HashSet<ClaimService> { claimService }
+                                });
+                            }
                         }
-                    }                       
-                }
-            }
-            var export = new AssociationRuleExport(@"c:\temp\transacoes\Prestador 1\", 61100016, true, null); // TMO
-            export.WriteToTransactionFile(claims2, out int total);
+                    }
 
-            Console.WriteLine($"Guias: { claims2.Count }, Serviços: { total }");            
+                    var export = new AssociationRuleExport(@"c:\temp\transacoes\Prestador 2\", i, false, null); // TMO
+                    export.WriteToTransactionFile(claims2, out int total);
+
+                    Console.WriteLine($"Guias: { claims2.Count }, Serviços: { total }");
+                    claims2 = new List<Claim>();
+                }                
+            }
+
+            Console.ReadLine();
         }
     }
 }
